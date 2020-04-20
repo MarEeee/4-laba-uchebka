da0df5d705290d1c98873e5219065ba503492892
MarEeee <pro100efim@gmail.com>
2020-04-20 14:55:58 +0500

    Добавьте файлы проекта.

---
+++ laba4_uchebka/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace laba4_uchebka
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.comboBoxForCity = new System.Windows.Forms.ComboBox();
+			this.comboBoxForHosp = new System.Windows.Forms.ComboBox();
+			this.comboBoxForPeople = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// comboBoxForCity
+			// 
+			this.comboBoxForCity.FormattingEnabled = true;
+			this.comboBoxForCity.Location = new System.Drawing.Point(39, 34);
+			this.comboBoxForCity.Name = "comboBoxForCity";
+			this.comboBoxForCity.Size = new System.Drawing.Size(187, 21);
+			this.comboBoxForCity.TabIndex = 0;
+			this.comboBoxForCity.SelectedIndexChanged += new System.EventHandler(this.comboBoxForCity_SelectedIndexChanged);
+			// 
+			// comboBoxForHosp
+			// 
+			this.comboBoxForHosp.FormattingEnabled = true;
+			this.comboBoxForHosp.Location = new System.Drawing.Point(39, 86);
+			this.comboBoxForHosp.Name = "comboBoxForHosp";
+			this.comboBoxForHosp.Size = new System.Drawing.Size(187, 21);
+			this.comboBoxForHosp.TabIndex = 1;
+			// 
+			// comboBoxForPeople
+			// 
+			this.comboBoxForPeople.FormattingEnabled = true;
+			this.comboBoxForPeople.Location = new System.Drawing.Point(39, 138);
+			this.comboBoxForPeople.Name = "comboBoxForPeople";
+			this.comboBoxForPeople.Size = new System.Drawing.Size(187, 21);
+			this.comboBoxForPeople.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(39, 195);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "label1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBoxForPeople);
+			this.Controls.Add(this.comboBoxForHosp);
+			this.Controls.Add(this.comboBoxForCity);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox comboBoxForCity;
+		private System.Windows.Forms.ComboBox comboBoxForHosp;
+		private System.Windows.Forms.ComboBox comboBoxForPeople;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ laba4_uchebka/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace laba4_uchebka
+{
+	
+	public partial class Form1 : Form
+	{
+		public string g;
+		public Form1()
+		{
+			InitializeComponent();
+			//блокировка ввода данных с клавиатуры в комбо бокс
+			comboBoxForCity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			comboBoxForHosp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			comboBoxForPeople.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+
+			using (var connect = new NpgsqlConnection("host = localhost; username = postgres; password = 1; database = postgres"))
+			{
+				connect.Open();				
+
+				DataSet MyTable = new DataSet();
+
+				//DataTable people = new DataTable("select firstname, secondname from people", "namesurname");
+				
+				//DataColumn combinedColumn = new DataColumn("FIO", typeof(string));
+				//combinedColumn.Expression = "[firstname] + ' ' + [secondname] ";
+				//people.Columns.Add(combinedColumn);
+				//MyTable.Tables.Add(people);
+
+				
+
+
+				var command = new NpgsqlCommand("select city_id, name from cities;", connect);
+				var command1 = new NpgsqlCommand("select id_hospital, hospital_name, c_id from hospitals;", connect);
+				var command2 = new NpgsqlCommand("select id_people,firstname, secondname, h_id from people", connect);
+
+				var adapter = new NpgsqlDataAdapter(command);
+				var adapter1 = new NpgsqlDataAdapter(command1);
+				var adapter2 = new NpgsqlDataAdapter(command2);
+
+				adapter.Fill(MyTable, "Cities");
+				adapter1.Fill(MyTable, "Hospitals");
+				adapter2.Fill(MyTable, "People");
+
+				DataRelation relation1 = MyTable.Relations.Add("CitiesHos",
+				  MyTable.Tables["cities"].Columns["city_id"],
+				   MyTable.Tables["hospitals"].Columns["c_id"]);
+
+				DataRelation relation2 = MyTable.Relations.Add("HospitalsPeople",
+					MyTable.Tables["hospitals"].Columns["id_hospital"],
+					MyTable.Tables["people"].Columns["h_id"]);
+
+
+				//MyTable.Relations.Add(relation1);
+				//MyTable.Relations.Add(relation2);
+
+				comboBoxForCity.DataSource = MyTable;
+				comboBoxForCity.DisplayMember = "cities.name";
+
+
+
+				comboBoxForHosp.DataSource = MyTable;
+				comboBoxForHosp.DisplayMember = "cities.CitiesHos.hospital_name";
+
+				comboBoxForPeople.DataSource = MyTable;
+				comboBoxForPeople.DisplayMember = "cities.CitiesHos.HospitalsPeople.secondname";
+			
+
+
+			}
+
+		}
+
+
+		private void comboBoxForCity_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			 g = comboBoxForCity.Text;
+			label1.Text = g;
+		
+
+
+		}
+	}
+}
